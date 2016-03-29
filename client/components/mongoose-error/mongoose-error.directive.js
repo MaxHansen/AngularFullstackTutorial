4954875290bd0621719f12bd68e098f4f24863f7
MaxHansen <v.6@live.dk>
2016-03-29 10:46:12 +0200

    initial scaffolding

---
+++ client/components/mongoose-error/mongoose-error.directive.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Removes server error when user updates input
+ */
+angular.module('gramesApp')
+  .directive('mongooseError', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModel) {
+        element.on('keydown', () => ngModel.$setValidity('mongoose', true));
+      }
+    };
+  });
